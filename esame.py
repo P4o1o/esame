d4643df25aa7e95290acc824266e00c9bc890d01
P4o1o <86971195+P4o1o@users.noreply.github.com>
2021-07-08 17:42:53 +0200

    Update esame.py

--- esame.py
+++ esame.py
@@ -1,3 +1,6 @@
+class ExamException(Exception):
+    pass
+
 class CSVTimeSeriesFile:
 
     def __init__(self, name):
@@ -16,6 +19,7 @@ class CSVTimeSeriesFile:
             raise ExamException('file "{}" non trovato'.format(self.name))
         # inizializzo la lista che sarà restituita dal metodo
         result = []
+        int i = 0
         # per ogni riga nel file
         for line in file:
             try:
@@ -25,14 +29,20 @@ class CSVTimeSeriesFile:
             except Exception as e:
                 # se ci sono campi vuoti o con meno di 2 colonne o righe non in formato csv ignoro la riga
                 continue
+            # controllo l'ordine degli epoch
+            if i > 0 and result[i - 1][0] >= lista[0]:
+                raise ExamException("epoch non ordinati all'interno del file csv")
             # aggiungo la lista [ora, misurazione] alla lista risultato
             result.append(lista)
+            i += 1
         # chiudo il file
         file.close()
         return result
 
 
 def compute_daily_max_difference(time_series):
+    if not isinstance(time_series, list):
+            raise ExamException('time series deve essere una lista, invece è "{}"'.format(type(time_series)))
     # inizializzo la lista che sarà restituita dal metodo
     result = []
     # inizializzo epoch del giorno precedente a quello considerato
@@ -42,6 +52,12 @@ def compute_daily_max_difference(time_series):
     # inizializzo la lista di temperature per il singolo giorno
     temperatures_series = []
     for i in range(len(time_series)):
+        if not isinstance(time_series[i][0], int):
+            raise ExamException('time series deve essere una lista di liste del tipo [epoch(int), temp(float/int)] su epoch ho "{}"'.format(type(time_series[i][0])))
+        if not (isinstance(time_series[i][1], float) or isinstance(time_series[i][1], int)):
+            raise ExamException('time series deve essere una lista di liste del tipo [epoch(int), temp(float/int)] su temp ho "{}"'.format(type(time_series[i][0])))
+        if i > 0 and (time_series[i][0] =< time_series[i - 1][0]):
+            raise ExamException('la time series non è ordinata')
         # trovo il giorno a cui appartiene l'epoche del giorno 'i'
         day_start_epoch = time_series[i][0] - (time_series[i][0] % 86400)
         # se si tratta del giorno successivo all'epoche precedentemente considerato
@@ -77,7 +93,3 @@ def compute_daily_max_difference(time_series):
         else:
             result.append(massimo - minimo)
     return result
-
-
-class ExamException(Exception):
-    pass
